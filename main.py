055a36a4636ef8a77ed11a44c0f06a68c82534be
Angelica Sanchez <angelica.sanchez@utec.edu.pe>
2021-09-24 01:27:54 -0500

    fixing functions

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def read_file(file = 'forestfires.csv'):
         data = csv.reader(csvfile, delimiter = ',')
         for line in data:
             if line[0] != 'X':
-                line[0] = NormalizeData(int(line[0]))
-                line[1] = NormalizeData(int(line[1]))
+                line[0] = normalize_data(int(line[0]))
+                line[1] = normalize_data(int(line[1]))
                 for i in range(4, 13):
                     if line[i] == '0':
                         line[i] = 0
@@ -25,24 +25,34 @@ def read_file(file = 'forestfires.csv'):
                 data_list.append(line)
     return data_list
 
+def update_months(dataframe):
+    dataframe.loc[dataframe["month"] == 'jan', "month"] = 1
+    dataframe.loc[dataframe["month"] == 'feb', "month"] = 2
+    dataframe.loc[dataframe["month"] == 'mar', "month"] = 3
+    dataframe.loc[dataframe["month"] == 'apr', "month"] = 4
+    dataframe.loc[dataframe["month"] == 'may', "month"] = 5
+    dataframe.loc[dataframe["month"] == 'jun', "month"] = 6
+    dataframe.loc[dataframe["month"] == 'jul', "month"] = 7
+    dataframe.loc[dataframe["month"] == 'aug', "month"] = 8
+    dataframe.loc[dataframe["month"] == 'sep', "month"] = 9
+    dataframe.loc[dataframe["month"] == 'oct', "month"] = 10
+    dataframe.loc[dataframe["month"] == 'nov', "month"] = 11
+    dataframe.loc[dataframe["month"] == 'dec', "month"] = 12
+
+def update_days(dataframe):
+    dataframe.loc[dataframe["day"] == 'mon', "day"] = 1
+    dataframe.loc[dataframe["day"] == 'tue', "day"] = 2
+    dataframe.loc[dataframe["day"] == 'wed', "day"] = 3
+    dataframe.loc[dataframe["day"] == 'thu', "day"] = 4
+    dataframe.loc[dataframe["day"] == 'fri', "day"] = 5
+    dataframe.loc[dataframe["day"] == 'sat', "day"] = 6
+    dataframe.loc[dataframe["day"] == 'sun', "day"] = 7
+
 def read_file_columns(file = 'forestfires.csv'):
-    data_list = []
     data = read_csv(file)
-    x = data['X'].tolist()
-    y = data['Y'].tolist()
-    month = data['month'].tolist()
-    day = data['day'].tolist()
-    ffmc = data['FFMC'].tolist()
-    dmc = data['DMC'].tolist()
-    dc = data['DC'].tolist()
-    isi = data['ISI'].tolist()
-    temp = data['temp'].tolist()
-    rh = data['RH'].tolist()
-    wind = data['wind'].tolist()
-    rain = data['rain'].tolist()
-    area = data['area'].tolist()       
-    data_list = {'X':x,'Y':y,'month':month,'day':day,'FFMC':ffmc,'DMC':dmc,'DC':dc,'ISI':isi,'temp':temp,'RH':rh,'wind':wind,'rain':rain,'area':area}
-    return data_list
+    update_months(data)
+    update_days(data)
+    return data
 
 def plot_data(data_x,data_y):
     plt.plot(data_x,data_y,'*')
@@ -64,42 +74,17 @@ def split_data(data):
             test.append(data[i])
     return train, validation, test
 
-def update_months(months):
-    months_new = []
-    for i in range(len(months)):
-        month = months[i]
-        if month=='jan': months_new.append(1)
-        if month=='feb': months_new.append(2)
-        if month=='mar': months_new.append(3)
-        if month=='apr': months_new.append(4)
-        if month=='may': months_new.append(5)
-        if month=='jun': months_new.append(6)
-        if month=='jul': months_new.append(7)
-        if month=='aug': months_new.append(8)
-        if month=='sep': months_new.append(9)
-        if month=='oct': months_new.append(10)
-        if month=='nov': months_new.append(11)
-        if month=='dec': months_new.append(12)
-    return months_new
+def normalize_features(x_features):
+    return (x_features-x_features.min())/(x_features.max()-x_features.min())
 
-def update_days(days):
-    months_new = []
-    for i in range(len(days)):
-        day = days[i]
-        if day=='mon': months_new.append(1)
-        if day=='tue': months_new.append(2)
-        if day=='wed': months_new.append(3)
-        if day=='thu': months_new.append(4)
-        if day=='fri': months_new.append(5)
-        if day=='sat': months_new.append(6)
-        if day=='sun': months_new.append(7)
-    return months_new
-        
 data = read_file()
-columns_data = read_file_columns()
-columns_data['month'] = update_months(columns_data['month'])
-columns_data['day'] = update_days(columns_data['day'])
-plot_data(columns_data['X'],columns_data['Y'])
 train, val, test = split_data(data)
+
+dataframe = read_file_columns()
+x_features = dataframe.iloc[:,:-1]
+y_values = dataframe.iloc[:,-1]
+x_features = normalize_features(x_features)
+print(x_features)
+
 
 
--- regression.py
+++ regression.py
@@ -1,33 +1,54 @@
+import numpy as np
 
-def h(w_list,x_list,b):
-    h_value = 0
-    return h_value
+x_train = []
+x_valid = []
+y_train = []
+y_valid = []
 
-def derivate(x_list,x_matrix,y_list,w_list,b,k):
+def h(x_features,w_values,b):
+    return w_values*x_features + b
+
+def derivate(x_features,y_values,w_values,b,k):
     sum1 = 0
     sum2 = 0
     dw_temp_list = []
-    for i in range(len(y_list)):
-        sum1 += (y_list[i] - h(x_list[i],w_list,b)) * (-1)
+    for i in range(len(y_values)):
+        sum1 += (y_values[i,:] - h(x_features[i,:],w_values,b)) * (-1)
 
     for j in range(k):
-        for i in range(len(y_list)):
-            sum2 += (y_list[i] - h(x_list[i],w_list,b)) *(-x_matrix[j][i])
+        for i in range(len(y_values)):
+            sum2 += (y_values[i,:] - h(x_features[i,:],w_values,b)) *(-x_features[i,j])
             dw_temp_list.append(sum2)
 
-    db = sum1 / len(y_list)
-    dw_list = [i/len(y_list) for i in dw_temp_list]
-    return dw_list, db
+    db = sum1 / len(y_values)
+    dw_values = [i/len(y_values) for i in dw_temp_list]
+    return dw_values, db
 
-def update(w_list,b,dw_list,db,alpha,k):
-    w_new = [w_list[j] - alpha*dw_list[j] for j in range(k)]
+def update(w_values,b,dw_values,db,alpha,k):
+    w_new = [w_values[j] - alpha*dw_values[j] for j in range(k)]
     b_new = b - alpha*db
     return w_new, b_new
 
-def error(x_list,y_list,w_list,b):
+def error(x_features,y_values,w_values,b):
     sum1 = 0
-    for i in range(len(y_list)):
-        sum1 += pow(y_list[i] - h(x_list[i],w_list,b), 2)
+    for i in range(len(y_values)):
+        sum1 += pow(y_values[i,:] - h(x_features[i,:],w_values,b), 2)
 
-    sum1 /= 2*len(y_list)
+    sum1 /= 2*len(y_values)
     return sum1
+
+def regression(alpha,p,epochs, k = 12):
+    error_train_list = []
+    error_valid_list = []
+    w_values = np.linspace(0, 1, num = p)
+    b = np.random.rand()
+    for i in range(epochs):
+        dw_values, db = derivate(x_train,y_train,w_values,b,k)
+        w, b = update(w_values,b,dw_values,db,alpha,k)
+        error_train_value = error(x_train,y_train,w_values,b)
+        error_valid_value = error(x_valid,y_valid,w_values,b)
+        error_train_list.append(error_train_value)
+        error_valid_list.append(error_valid_value)
+        if i > 20000:
+            break
+    return error_train_list,error_valid_list
