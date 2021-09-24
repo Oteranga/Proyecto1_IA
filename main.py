d38e63de088f87b66965f4607da22241696a7ea5
Angelica Sanchez <angelica.sanchez@utec.edu.pe>
2021-09-24 15:16:57 -0500

    splitting train,valid,test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
+from os import X_OK
 import numpy as np
 import matplotlib.pyplot as plt
-#from sklearn.model_selection import train_test_split
 import random
 import math
 from pandas import *
@@ -25,28 +25,28 @@ def read_file(file = 'forestfires.csv'):
                 data_list.append(line)
     return data_list
 
-def update_months(dataframe):
-    dataframe.loc[dataframe["month"] == 'jan', "month"] = 1
-    dataframe.loc[dataframe["month"] == 'feb', "month"] = 2
-    dataframe.loc[dataframe["month"] == 'mar', "month"] = 3
-    dataframe.loc[dataframe["month"] == 'apr', "month"] = 4
-    dataframe.loc[dataframe["month"] == 'may', "month"] = 5
-    dataframe.loc[dataframe["month"] == 'jun', "month"] = 6
-    dataframe.loc[dataframe["month"] == 'jul', "month"] = 7
-    dataframe.loc[dataframe["month"] == 'aug', "month"] = 8
-    dataframe.loc[dataframe["month"] == 'sep', "month"] = 9
-    dataframe.loc[dataframe["month"] == 'oct', "month"] = 10
-    dataframe.loc[dataframe["month"] == 'nov', "month"] = 11
-    dataframe.loc[dataframe["month"] == 'dec', "month"] = 12
+def update_months(df):
+    df.loc[df["month"] == 'jan', "month"] = 1
+    df.loc[df["month"] == 'feb', "month"] = 2
+    df.loc[df["month"] == 'mar', "month"] = 3
+    df.loc[df["month"] == 'apr', "month"] = 4
+    df.loc[df["month"] == 'may', "month"] = 5
+    df.loc[df["month"] == 'jun', "month"] = 6
+    df.loc[df["month"] == 'jul', "month"] = 7
+    df.loc[df["month"] == 'aug', "month"] = 8
+    df.loc[df["month"] == 'sep', "month"] = 9
+    df.loc[df["month"] == 'oct', "month"] = 10
+    df.loc[df["month"] == 'nov', "month"] = 11
+    df.loc[df["month"] == 'dec', "month"] = 12
 
-def update_days(dataframe):
-    dataframe.loc[dataframe["day"] == 'mon', "day"] = 1
-    dataframe.loc[dataframe["day"] == 'tue', "day"] = 2
-    dataframe.loc[dataframe["day"] == 'wed', "day"] = 3
-    dataframe.loc[dataframe["day"] == 'thu', "day"] = 4
-    dataframe.loc[dataframe["day"] == 'fri', "day"] = 5
-    dataframe.loc[dataframe["day"] == 'sat', "day"] = 6
-    dataframe.loc[dataframe["day"] == 'sun', "day"] = 7
+def update_days(df):
+    df.loc[df["day"] == 'mon', "day"] = 1
+    df.loc[df["day"] == 'tue', "day"] = 2
+    df.loc[df["day"] == 'wed', "day"] = 3
+    df.loc[df["day"] == 'thu', "day"] = 4
+    df.loc[df["day"] == 'fri', "day"] = 5
+    df.loc[df["day"] == 'sat', "day"] = 6
+    df.loc[df["day"] == 'sun', "day"] = 7
 
 def read_file_columns(file = 'forestfires.csv'):
     data = read_csv(file)
@@ -58,33 +58,25 @@ def plot_data(data_x,data_y):
     plt.plot(data_x,data_y,'*')
     plt.show()
 
-def split_data(data):
-    # x_train, x_rest, y_train, y_rest = train_test_split(x,y,train_size=0.7, shuffle=True)
-    # x_valid, x_test, y_valid, y_test = train_test_split(x_rest,y_rest,test_size=0.33)
-    train = []
-    validation = []
-    test = []
-    random.shuffle(data)
-    for i in range(len(data)):
-        if i <= math.ceil(len(data) * 0.7):
-            train.append(data[i])
-        elif i > math.ceil(len(data) * 0.7) and i <= math.floor(len(data) * 0.9):
-            validation.append(data[i])
-        else:
-            test.append(data[i])
-    return train, validation, test
-
 def normalize_features(x_features):
     return (x_features-x_features.min())/(x_features.max()-x_features.min())
 
-data = read_file()
-train, val, test = split_data(data)
+def get_x_y(df):
+    x_features = df.iloc[:,:-1]
+    y_values = df.iloc[:,-1]
+    x_features = normalize_features(x_features)
+    return x_features,y_values
 
-dataframe = read_file_columns()
-x_features = dataframe.iloc[:,:-1]
-y_values = dataframe.iloc[:,-1]
-x_features = normalize_features(x_features)
-print(x_features)
+
+data = read_file()
+
+df = read_file_columns()
+train_df, valid_df, test_df = np.split(df.sample(frac=1, random_state=42),[int(math.ceil(.7*len(df))), int(math.floor(.9*len(df)))])
+
+x_train_features,y_train_values = get_x_y(train_df)
+x_valid_features,y_valid_values = get_x_y(valid_df)
+x_test_features,y_test_values = get_x_y(test_df)
+
 
 
 
